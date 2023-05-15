c9fb68921a4be363c8faeb0f40d8410917a8015f
yoshiko <ttkmysk021@gmail.com>
2023-05-15 21:27:59 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// 1
+var resultNum = 10 > 8 ? true : false;
+console.log(resultNum);
+
+// 2
+var resultNum = 10 >= 5 ? true : false;
+console.log(resultNum);
+
+var resultNum = 4 >= 5 ? true : false;
+console.log(resultNum);
+
+// 3
+var resultNum = 18 >= 5 ? true : false;
+console.log(resultNum);
+
+var resultNum = 2 >= 5 ? true : false;
+console.log(resultNum);
+
+// 4
+var resultNum = 10 > 5 ? true : false;
+console.log(resultNum);
+
+var resultNum = 2 > 10 ? true : false;
+console.log(resultNum);
+
+// 5
+var resultNum = -99 ? true : false;
+console.log(resultNum);
+
+
+var resultNum = null ? true : false;
+console.log(resultNum);
