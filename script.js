46e5f0231424298b4fdc7455d887ea59852d9f91
yoshiko <ttkmysk021@gmail.com>
2023-05-30 12:30:27 +0900

    second

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-// 1
+// 1　変数の中にtrueまたは、falseが代入された時の処理を作ってください。
 var resultNum = 10 > 8 ? true : false;
 console.log(resultNum);
 
-// 2
+// 2　代入された変数の中が5より大きい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。
 var resultNum = 10 >= 5 ? true : false;
 console.log(resultNum);
 
 var resultNum = 4 >= 5 ? true : false;
 console.log(resultNum);
 
-// 3
+// 3　代入された変数の中が5より小さい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。
 var resultNum = 18 >= 5 ? true : false;
 console.log(resultNum);
 
 var resultNum = 2 >= 5 ? true : false;
 console.log(resultNum);
 
-// 4
+// 4代入された変数の中が10以上の時にtrueの処理と、10以下の時にfalseの処理を作ってください。
 var resultNum = 10 > 5 ? true : false;
 console.log(resultNum);
 
 var resultNum = 2 > 10 ? true : false;
 console.log(resultNum);
 
-// 5
+// 5　代入された変数の中に値が存在する場合はtrue、存在しない場合はfalseの処理にしてください。
 
+
+// var resultNum = -99  ? true : false;
+// console.log(resultNum);
 
 var resultNum = 5 ? true : false;
 console.log(resultNum);
